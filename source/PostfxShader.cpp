4e20f83029e7bddca1207f54fb0352139dc34ea0
Tomashu <tomash4my@gmail.com>
2019-11-02 12:49:18 +0100

    moved textures to postfx_shader

--- source/PostfxShader.cpp
+++ source/PostfxShader.cpp
@@ -2,10 +2,11 @@
 #include "EngineCore.h"
 #include "PostfxShader.h"
 #include "Render.h"
+#include "Engine.h"
 #include "DirectX.h"
 
 //=================================================================================================
-PostfxShader::PostfxShader() : effect(nullptr)
+PostfxShader::PostfxShader() : effect(nullptr), surf(), tex()
 {
 }
 
@@ -24,6 +25,8 @@ void PostfxShader::OnInit()
 	hPower = effect->GetParameterByName(nullptr, "power");
 	hSkill = effect->GetParameterByName(nullptr, "skill");
 	assert(hTex && hPower && hSkill);
+
+	CreateTextures();
 }
 
 //=================================================================================================
@@ -31,6 +34,11 @@ void PostfxShader::OnReset()
 {
 	if(effect)
 		V(effect->OnLostDevice());
+	for(int i = 0; i < 3; ++i)
+	{
+		SafeRelease(surf[i]);
+		SafeRelease(tex[i]);
+	}
 }
 
 //=================================================================================================
@@ -38,10 +46,46 @@ void PostfxShader::OnReload()
 {
 	if(effect)
 		V(effect->OnResetDevice());
+	CreateTextures();
 }
 
 //=================================================================================================
 void PostfxShader::OnRelease()
 {
 	SafeRelease(effect);
+	for(int i = 0; i < 3; ++i)
+	{
+		SafeRelease(surf[i]);
+		SafeRelease(tex[i]);
+	}
+}
+
+//=================================================================================================
+void PostfxShader::CreateTextures()
+{
+	if(tex[0])
+		return;
+
+	IDirect3DDevice9* device = app::render->GetDevice();
+	const Int2& wnd_size = app::engine->GetWindowSize();
+	int ms, msq;
+	app::render->GetMultisampling(ms, msq);
+	D3DMULTISAMPLE_TYPE type = (D3DMULTISAMPLE_TYPE)ms;
+	if(ms != D3DMULTISAMPLE_NONE)
+	{
+		for(int i = 0; i < 3; ++i)
+		{
+			V(device->CreateRenderTarget(wnd_size.x, wnd_size.y, D3DFMT_X8R8G8B8, type, msq, FALSE, &surf[i], nullptr));
+			tex[i] = nullptr;
+		}
+		V(device->CreateTexture(wnd_size.x, wnd_size.y, 1, D3DUSAGE_RENDERTARGET, D3DFMT_X8R8G8B8, D3DPOOL_DEFAULT, &tex[0], nullptr));
+	}
+	else
+	{
+		for(int i = 0; i < 3; ++i)
+		{
+			V(device->CreateTexture(wnd_size.x, wnd_size.y, 1, D3DUSAGE_RENDERTARGET, D3DFMT_X8R8G8B8, D3DPOOL_DEFAULT, &tex[i], nullptr));
+			surf[i] = nullptr;
+		}
+	}
 }
