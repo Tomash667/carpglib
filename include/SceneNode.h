5261d1ee9af926f49a12e0dbb4fe1a9bd51aa3be
Tomashu <tomash4my@gmail.com>
2019-11-03 21:48:59 +0100

    stun effect as node

--- include/SceneNode.h
+++ include/SceneNode.h
@@ -16,7 +16,9 @@ struct SceneNode
 		F_NORMAL_MAP = 1 << 3,
 		F_NO_ZWRITE = 1 << 4,
 		F_NO_CULLING = 1 << 5,
-		F_ALPHA_TEST = 1 << 6
+		F_ALPHA_TEST = 1 << 6,
+		F_ALPHA_BLEND = 1 << 7,
+		F_NO_LIGHTING = 1 << 8
 	};
 
 	Matrix mat;
@@ -26,9 +28,11 @@ struct SceneNode
 		MeshInstance* mesh_inst;
 	};
 	MeshInstance* parent_mesh_inst;
+	float dist;
 	int flags, lights, subs;
 	const TexOverride* tex_override;
 	Vec4 tint;
+	Vec3 pos;
 	bool billboard;
 
 	const Mesh& GetMesh() const
