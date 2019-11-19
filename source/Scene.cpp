0674b56efbbab6e8673a77db082c862a90ebd551
Tomashu <tomash4my@gmail.com>
2019-11-19 09:14:52 +0100

    scene contain nodes

--- source/Scene.cpp
+++ source/Scene.cpp
@@ -1,3 +1,10 @@
 #include "EnginePch.h"
 #include "EngineCore.h"
 #include "Scene.h"
+#include "SceneNode.h"
+
+//=================================================================================================
+void Scene::OnFree()
+{
+	SceneNode::Free(nodes);
+}
