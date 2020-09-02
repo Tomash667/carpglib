147ee49d6af5e021f4d417078545a3bf84aa9dfe
Tomashu <tomash4my@gmail.com>
2020-09-02 14:23:06 +0200

    quadtree wip

--- include/QuadtreeScene.h
+++ include/QuadtreeScene.h
@@ -1,8 +1,19 @@
 #pragma once
 
+//-----------------------------------------------------------------------------
+#include <QuadTree.h>
 #include <Scene.h>
 
+//-----------------------------------------------------------------------------
 struct QuadtreeScene : public Scene
 {
-
+	QuadtreeScene();
+	~QuadtreeScene();
+	void Add(SceneNode* node) override;
+	void Remove(SceneNode* node) override;
+	void Clear() override;
+	void ListNodes(SceneBatch& batch) override;
+private:
+	vector<SceneNode*> nodes;
+	QuadTree quadtree;
 };
---
+++ source/QuadtreeScene.cpp
@@ -0,0 +1,2 @@
+#include "Pch.h"
+#include "QuadtreeScene.h"
