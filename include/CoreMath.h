8924eb98c50ca178acc93eebd2fb4c117e6d2fb7
Tomashu <tomash4my@gmail.com>
2024-08-13 15:36:38 +0200

    CoreMath const -> constexpr

--- include/CoreMath.h
+++ include/CoreMath.h
@@ -20,10 +20,10 @@ struct Guid;
 
 //-----------------------------------------------------------------------------
 // Global constants
-const float PI = 3.14159265358979323846f;
-const float SQRT_2 = 1.41421356237f;
-const float G = 9.8105f;
-const float MAX_ANGLE = PI * 2 - FLT_EPSILON;
+constexpr float PI = 3.14159265358979323846f;
+constexpr float SQRT_2 = 1.41421356237f;
+constexpr float G = 9.8105f;
+constexpr float MAX_ANGLE = PI * 2 - FLT_EPSILON;
 
 //-----------------------------------------------------------------------------
 // Random numbers
